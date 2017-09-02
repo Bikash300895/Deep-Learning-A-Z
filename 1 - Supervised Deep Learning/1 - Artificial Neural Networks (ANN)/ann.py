5963376fd19def50af73a459f55d4bc7747bdb91
Bikash <bikash11roy@gmail.com>
2017-09-03 00:37:43 +0600

    neural network trained
    Signed-off-by: Bikash <bikash11roy@gmail.com>

--- 1 - Supervised Deep Learning/1 - Artificial Neural Networks (ANN)/ann.py
+++ 1 - Supervised Deep Learning/1 - Artificial Neural Networks (ANN)/ann.py
@@ -1,3 +1,5 @@
+""" Step 1: Data preprocessing"""
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -20,3 +22,38 @@ X[:, 2] = label_encoder_2.fit_transform(X[:, 2])
 # one hot encoder
 one_hot = OneHotEncoder(categorical_features= [1])
 X = one_hot.fit_transform(X).toarray()
+
+# Removing one variable of one hot encoder to avoid dummy variable trup
+X = X[:, 1:]
+
+
+# Splitting the data into train and test data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_trian, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
+
+
+
+""" Step 2: Meking the deep learning model"""
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Lets start building the neural network
+# 1. defining ther neural network itself
+classifier = Sequential()
+
+# 2. Add the first hidden layer
+# as a rule of thumb we will take the dimension as (input_dim + output_dim)/2
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
+
+
+
+
+
+
+
