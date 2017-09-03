28df25188fa11936772b47a27a10e4421d2e8b34
Bikash <bikash11roy@gmail.com>
2017-09-03 23:18:51 +0600

    Adding the layers in the CNN
    Signed-off-by: Bikash <bikash11roy@gmail.com>

--- 1 - Supervised Deep Learning/2 - Convolutional Neural Networks (CNN)/CNN.py
+++ 1 - Supervised Deep Learning/2 - Convolutional Neural Networks (CNN)/CNN.py
@@ -1,6 +1,27 @@
 from keras.models import Sequential
-from keras.layers import Convolution2D
+from keras.layers import Conv2D
 from keras.layers import Flatten
 from keras.layers import MaxPooling2D
 from keras.layers import Dense
+
+# initialize the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
+
+# Step 2 - Polling
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Fully connected layer
+classifier.add(Dense(units=128, activation='relu'))
+classifier.add(Dense(units=1, activation='sigmoid'))
+
+
+
+
+
 
