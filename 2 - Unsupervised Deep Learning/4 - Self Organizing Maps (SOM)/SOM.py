11d5ed448feede1e1cb113d5d4952eff955c3c5c
Bikash <bikash11roy@gmail.com>
2017-09-09 06:03:43 +0600

    SOM trained
    Signed-off-by: Bikash <bikash11roy@gmail.com>

--- 2 - Unsupervised Deep Learning/4 - Self Organizing Maps (SOM)/SOM.py
+++ 2 - Unsupervised Deep Learning/4 - Self Organizing Maps (SOM)/SOM.py
@@ -4,3 +4,16 @@ import pandas as pd
 
 # import the dataset
 dataset = pd.read_csv('Credit_Card_Applications.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Fearure scalling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range=(0, 1))
+X = sc.fit_transform(X)
+
+# Training the SOM
+from minisom import MiniSom
+som = MiniSom(x = 10, y = 10, input_len=15, sigma = 1.0, learning_rate=0.5)
+som.random_weights_init(X)
+som.train_random(data=X, num_iteration=100)
